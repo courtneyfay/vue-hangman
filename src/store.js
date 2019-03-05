ffdfe8fa7375b3ea4f6d9246d607a63978bb297f
Courtney Fay <fay.courtney@gmail.com>
2019-03-04 20:13:07 -0700

    getting guesses to return from the API!

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@ import Vuex from 'vuex'
 import axios from 'axios'
 const baseUrl = 'http://flask-hangman-api.herokuapp.com'
 
+axios.defaults.withCredentials = true
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -35,35 +37,21 @@ export default new Vuex.Store({
       })
     },
     postGuess({ commit }, letter) {
-      const path = `/guess/${letter}`
-      const http = axios.create({
-        baseUrl,
-        headers: { 'Cache-Control': 'no-cache' },
-        PostmanToken: 'bd386705-da6b-5bd9-b4df-f68b6e48a70b'
-        //Cache-Control: no-store, no-cache, no-transform, must-revalidate, max-age=0
-      })
-      //using http://10.0.9.0:8080/sockjs-node/info?t=1550720569625
-      //GET /guess/a HTTP/1.1
-      // Host: flask-hangman-api.herokuapp.com
-      // Cache-Control: no-cache
-      // Postman-Token: 1bb3a3e4-b052-e6dc-9d11-c9d2e1ac8d2c
-      // 400 error
-      // {
-      //   "code": "no_game_in_progress",
-      //   "message": "Couldn't find a game for you. Please visit /new_game"
-      // }
+      const url = `${baseUrl}/guess/${letter}`
+      axios
+        .get(url)
+        .then(response => {
+          const guesses = response.data.new_state.guesses
+          const wordToGuess = response.data.new_state.word_so_far
 
-      http.get(path).then(response => {
-        console.log('response', response)
+          console.log('response', response.data)
 
-        // const wordToGuess = response.data.new_state.word_so_far
-        // const guesses = response.data.new_state.guesses
-        // const status = response.data
-
-        // commit('updateWordToGuess', wordToGuess)
-        // commit('updateGuesses', guesses)
-        // commit('updateGameStatus', status)
-      })
+          commit('updateWordToGuess', wordToGuess)
+          commit('updateGuesses', guesses)
+        })
+        .catch(err => {
+          console.error(err)
+        })
     }
   }
 })
